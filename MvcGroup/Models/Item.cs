3513b1988808709815392a6c5def41a67ad32a0b
Foster Stule <camodude5@yahoo.com>
2018-06-29 12:25:56 -0400

    Added item and user classes

---
+++ MvcGroup/Models/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcGroup.Models
+{
+	public class Item
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public bool isFinished { get; set; }
+		public int UserId { get; set; }
+		public virtual User User { get; set; }
+
+		public Item() { }
+	}
+}
--- MvcGroup/Models/User.cs
+++ MvcGroup/Models/User.cs
@@ -7,5 +7,12 @@ namespace MvcGroup.Models
 {
 	public class User
 	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Username { get; set; }
+		public string Password { get; set; }
+		public string Email { get; set; }
+
+		public User() { }
 	}
 }
