b0dcf3c0c3d9eaf4e49ca1b122a00b3505378749
Foster Stule <camodude5@yahoo.com>
2018-06-29 13:58:14 -0400

    added controllers and the migration to the database

---
+++ MvcGroup/Models/GroupContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcGroup.Models
+{
+	public class GroupContext : DbContext
+	{
+		public GroupContext() : base() {}
+
+
+		public DbSet<User> Users { get; set; }
+		public DbSet<Item> Items { get; set; }
+	}
+}
--- MvcGroup/Models/Item.cs
+++ MvcGroup/Models/Item.cs
@@ -8,12 +8,12 @@ namespace MvcGroup.Models
 	public class Item
 	{
 		public int Id { get; set; }
-		public string Title { get; set; }
+		public string Task { get; set; }
 		public string Description { get; set; }
-		public bool isFinished { get; set; }
+		public bool IsFinished { get; set; }
 		public int UserId { get; set; }
 		public virtual User User { get; set; }
-
+		public DateTime Date { get; set; }
 		public Item() { }
 	}
 }
